59c37bc320b676ba13e3a930cfe1ca71ca768cad
almamun1740 <mamun333089@gmail.com>
2021-04-03 22:07:54 +0600

    mamun

---
+++ valid_parentheses.go
@@ -0,0 +1,16 @@
+package kata
+
+func ValidParentheses(parens string) bool {
+	count := 0
+	for _, c := range parens {
+		if c == '(' {
+			count++
+		} else {
+			count--
+		}
+		if count < 0 {
+			return false
+		}
+	}
+	return count == 0
+}
